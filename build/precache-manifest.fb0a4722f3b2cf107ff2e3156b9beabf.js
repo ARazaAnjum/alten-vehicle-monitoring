2c92fae52508ffe85193c12eb1eacb2f3d07a3ab
Ali Raza <arazaanjum@gmail.com>
2019-11-23 12:17:01 +0500

    Updated build, wireframe and user flow/use case diagrams

---
+++ build/precache-manifest.fb0a4722f3b2cf107ff2e3156b9beabf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1bdf50c364102580bb85a5061fe787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a992ae26567efcc5705",
+    "url": "/static/css/main.cc69679f.chunk.css"
+  },
+  {
+    "revision": "261d6b08d9c4d93568a6",
+    "url": "/static/js/2.e44c8300.chunk.js"
+  },
+  {
+    "revision": "9a992ae26567efcc5705",
+    "url": "/static/js/main.0b647ad4.chunk.js"
+  },
+  {
+    "revision": "67b2ad2ab79c65b06cef",
+    "url": "/static/js/runtime-main.1d3a968c.js"
+  }
+]);
